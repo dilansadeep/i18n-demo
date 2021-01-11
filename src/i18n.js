9e36bf0fe3347e21fb913114d1938630841e6d17
Dilan Tennegedara <dilan.tennegedara@gmail.com>
2021-01-12 01:09:06 +0530

    add localStorage as primary cache

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,11 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-
-import Backend from 'i18next-http-backend';
-import Url from "url-parse"
+import i18n from "i18next";
+import Backend from "i18next-chained-backend";
+import LocalStorageBackend from 'i18next-localstorage-backend'; // primary use cache
+import HttpApi from "i18next-http-backend"; // fallback http load
+import { initReactI18next } from "react-i18next";
+import Url from "url-parse";
 // don't want to use this?
-// have a look at the Quick start guide 
+// have a look at the Quick start guide
 // for passing in lng and translations on init
 
 i18n
@@ -18,31 +19,44 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: 'en',
+    fallbackLng: "en",
     debug: true,
     backend: {
-        crossDomain: true,
-        withCredentials: true,
-        customHeaders: {},
-        queryStringParams: { v: '1.3.5' },
-        loadPath(langs, namespaces) {
-            // @ToDo: Add multi-loading support
-            const [lang] = langs
-            let [namespace] = namespaces
-            if (namespace === "translation") namespace = "pages/translation"
-            const url = new Url(`https://dev-noetic1-febe.imperialhotels.co.uk/api/content/${namespace}`, true)
+      backends: [
+        LocalStorageBackend,  // primary
+        HttpApi // fallback
+      ],
+      backendOptions: [
+        {
+            expirationTime: 7*24*60*60*1000,
+            defaultVersion: 'v1.2'
+        },
+        {
+          crossDomain: true,
+          withCredentials: true,
+          customHeaders: {},
+          queryStringParams: { v: "1.3.5" },
+          loadPath(langs, namespaces) {
+            const [lang] = langs;
+            let [namespace] = namespaces;
+            if (namespace === "translation") namespace = "pages/translation";
+            const url = new Url(
+              `https://dev-noetic1-febe.imperialhotels.co.uk/api/content/${namespace}`,
+              true
+            );
             const query = {
-                ...url.query,
-                language: lang
-            }
-            url.set("query", query)
-            return url.toString()
-        }
+              ...url.query,
+              language: lang,
+            };
+            url.set("query", query);
+            return url.toString();
+          },
+        },
+      ],
     },
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
-    }
+    },
   });
-
 
 export default i18n;
